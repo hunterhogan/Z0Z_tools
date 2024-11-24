0c6ed008b655394e2027ceeca97fae09eb6edc2c
Hunter Hogan <hunterhogan@users.noreply.github.com>
2024-11-24 15:44:04 -0500

    Update Z0Z_tools package metadata and dependencies; remove setup.cfg; bump version to 0.5.2

--- Z0Z_tools/__init__.py
+++ Z0Z_tools/__init__.py
@@ -1,9 +1,16 @@
-"""Z0Z_tools package."""
-from importlib.metadata import metadata
+"""Z0Z_tools package for reusable tools across various projects."""
 
-metadataPackage = metadata('Z0Z_tools')
-__version__ = metadataPackage['Version']
-__author__ = metadataPackage['Author']
+__version__ = "0.5.2"
+__author__ = "Hunter Hogan"
+__description__ = "Audio processing, data structure, and package management tools."
+__url__ = "https://github.com/hunterhogan/Z0Z_tools"
+__dependencies__ = [
+    "numpy",
+    "packaging",
+    "samplerate @ git+https://github.com/tuxu/python-samplerate.git@fix_cmake_dep ; platform_system == 'Linux' and platform_machine == 'x86_64' and python_version <= '3.10'",
+    "samplerate",
+    "soundfile"
+]
 
 from Z0Z_tools.pipAnything import installPackageTarget, makeListRequirementsFromRequirementsFile
 from Z0Z_tools.Z0Z_dataStructure import stringItUp, updateExtendPolishDictionaryLists
