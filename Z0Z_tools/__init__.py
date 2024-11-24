49c5d3991d3f44028f624f251186a74915c5bc44
Hunter Hogan <hunterhogan@users.noreply.github.com>
2024-11-24 16:19:37 -0500

    Refactor package dependencies: move dependencies from __init__.py to pyproject.toml

--- Z0Z_tools/__init__.py
+++ Z0Z_tools/__init__.py
@@ -1,13 +1,6 @@
 __author__ = "Hunter Hogan"
 __url__ = "https://github.com/hunterhogan/Z0Z_tools"
 __version__ = "0.5.2"
-__dependencies__ = [
-    "numpy",
-    "packaging",
-    "samplerate @ git+https://github.com/tuxu/python-samplerate.git@fix_cmake_dep ; platform_system == 'Linux' and platform_machine == 'x86_64' and python_version <= '3.10'",
-    "samplerate",
-    "soundfile"
-]
 
 from Z0Z_tools.pipAnything import installPackageTarget, makeListRequirementsFromRequirementsFile
 from Z0Z_tools.Z0Z_dataStructure import stringItUp, updateExtendPolishDictionaryLists
