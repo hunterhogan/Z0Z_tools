040fd7e588aa9e5f734a96b16a55445016dd94af
Hunter Hogan <hunterhogan@users.noreply.github.com>
2024-11-24 16:14:58 -0500

    Add description to package metadata in pyproject.toml and remove from __init__.py

--- Z0Z_tools/__init__.py
+++ Z0Z_tools/__init__.py
@@ -1,5 +1,4 @@
 __author__ = "Hunter Hogan"
-__description__ = "Audio processing, data structure, and package management tools."
 __url__ = "https://github.com/hunterhogan/Z0Z_tools"
 __version__ = "0.5.2"
 __dependencies__ = [
