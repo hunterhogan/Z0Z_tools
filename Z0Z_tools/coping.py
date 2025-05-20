e35bcb77a2d9ca4e3eccd24267c18ff0b27320e5
Hunter Hogan <hunterhogan@users.noreply.github.com>
2025-05-20 13:18:30 -0500

    Add docstring to acknowledge contributions and discussions

--- Z0Z_tools/coping.py
+++ Z0Z_tools/coping.py
@@ -6,3 +6,7 @@ def raiseIfNone(returnTarget: TypeSansNone | None) -> TypeSansNone:
     if returnTarget is None:
         raise ValueError('Return is None.')
     return returnTarget
+"""
+Thanks to https://github.com/sobolevn.
+https://github.com/python/typing/discussions/1997#discussioncomment-13108399
+"""
